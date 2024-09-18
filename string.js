2c87eca9163ecfd446e7e6768d9c50369eeb49bd
Alina <hrusinskaalinka@gmail.com>
2024-09-18 23:52:13 +0300

    dz3

---
+++ string.js
@@ -0,0 +1,23 @@
+// 1.
+
+const result = 5 + 5 + "5";
+console.log(typeof result);
+
+// 2.
+
+const email = "hrusinskaalinka@gmail.com";
+console.log(email.includes("@"), email.length);
+
+// 3.
+
+const one = "My";
+const two = "name";
+const three = "is";
+const fullName = `${one} ${two} ${three} Victor`;
+console.log(fullName);
+
+// 4.
+
+const userName = "Oleg";
+const payment = alert(`Дякуємо, Олег! До сплати 300 гривень`);
+console.log(payment);
